4ea1c258c84d68fbdcf3014a38e5442ba608d75f
Kris Zhang <kriszhang122507@gmail.com>
2026-06-04 13:15:44 +1200

    Continue with map development
    Started complete rebuild of Mackillop campus due to severe collision shape creation errors and size issues

--- main-game/MainMenu/Scripts/main_menu.gd
+++ main-game/MainMenu/Scripts/main_menu.gd
@@ -14,3 +14,7 @@ func _process(delta: float) -> void:
 
 func _on_start_pressed() -> void:
 	get_tree().change_scene_to_file("res://Main_Game/Scenes/MainMap.tscn")
+
+
+func _on_quit_pressed() -> void:
+	get_tree().quit()
