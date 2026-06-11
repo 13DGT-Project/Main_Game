2544be1a656d5a092d13c5c89927d463ea450016
Kris Zhang <kriszhang122507@gmail.com>
2026-06-12 01:38:00 +1200

    Add settings scene; adjust sky and project configs
    Add a new Settings scene. Update project.godot to set viewport width to 1920 and add 3D default gravity = 9.81.

---
+++ main-game/Backend/Settings/Scripts/settings.gd
@@ -0,0 +1,19 @@
+extends Node2D
+
+
+
+
+func _on_home_pressed() -> void:
+	pass # Replace with function body.
+
+
+
+
+func _on_language_pressed() -> void:
+	pass # Replace with function body.
+
+
+
+
+func _on_exit_settings_pressed() -> void:
+	get_tree().change_scene_to_file("res://MainMenu/Scenes/main_menu.tscn")
