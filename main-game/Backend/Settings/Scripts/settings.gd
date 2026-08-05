0cca802c10bd697a780a909aa337c6f0057fb8c8
Kris Zhang <kriszhang122507@gmail.com>
2026-08-05 12:21:34 +1200

    Add localization and replace hardcoded UI strings
    Introduce localization support: add CSV + .translation files and importer for English/Chinese/Japanese/Spanish. Replace hardcoded UI text in main menu, credits, instructions, and settings with translation keys. Add language selection buttons/panels and handlers in settings.gd (including visual selection highlighting) and set default locale to "en". Register translations in project.godot and make minor settings UI visibility/layout tweaks.

--- main-game/Backend/Settings/Scripts/settings.gd
+++ main-game/Backend/Settings/Scripts/settings.gd
@@ -4,8 +4,13 @@ extends Node2D
 @onready var home_panel = $CanvasLayer/Control/Home_System
 @onready var home_node = $CanvasLayer/Control/Home
 @onready var language_node = $CanvasLayer/Control/Languages
+@onready var language_panel_english = $CanvasLayer/Control/Languages/English
+@onready var language_panel_chinese = $CanvasLayer/Control/Languages/Chinese
+@onready var language_panel_japanese = $CanvasLayer/Control/Languages/Japanese
+@onready var language_panel_spanish = $CanvasLayer/Control/Languages/Spanish
 
-
+func _ready() -> void:
+	TranslationServer.set_locale("en")
 
 func _on_home_pressed() -> void:
 	var current_style = language_panel.get_theme_stylebox("panel")
@@ -47,3 +52,35 @@ func _on_language_pressed() -> void:
 
 func _on_exit_settings_pressed() -> void:
 	get_tree().change_scene_to_file("res://MainMenu/Scenes/main_menu.tscn")
+
+
+func set_selected_language(selected_panel: Panel):
+	var panels = [
+		language_panel_english,
+		language_panel_chinese,
+		language_panel_japanese,
+		language_panel_spanish
+	]
+
+	for panel in panels:
+		var style = panel.get_theme_stylebox("panel").duplicate()
+
+		if panel == selected_panel:
+			style.bg_color = Color("#8d8d8dde") # Selected
+		else:
+			style.bg_color = Color("#262626") # Unselected
+
+		panel.add_theme_stylebox_override("panel", style)
+
+func _on_english_pressed() -> void:
+	set_selected_language(language_panel_english)
+
+
+func _on_chinese_pressed() -> void:
+	set_selected_language(language_panel_chinese)
+
+func _on_japanese_pressed() -> void:
+	set_selected_language(language_panel_japanese)
+
+func _on_spanish_pressed() -> void:
+	set_selected_language(language_panel_spanish)
