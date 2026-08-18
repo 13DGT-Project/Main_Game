a411a08550d364b1ef70ce86b02fceb99dfadb72
Kris Zhang <kriszhang122507@gmail.com>
2026-08-18 15:03:17 +1200

    Add university selection backend and UI
    Introduces a new `GameBackend` autoload to store selected university data and wires the intro university scene to preview/select Auckland or Canterbury before entering the main map. Updates the GUI to display selected university info and adds new university image assets/imports. The change set also includes scene updates such as adding the player group tag, embedding a pause menu in `n_block`, and environment/layout adjustments in main map and related scenes.

---
+++ main-game/Backend/GameBackend.gd
@@ -0,0 +1,26 @@
+extends Node
+
+
+var selected_university: String = ""
+
+# GameBackend.universities[GameBackend.selected_university]
+
+var universities = {
+	"Auckland": {
+		"money_needed": 35000,
+		"grades_needed": 80,
+		"location": "Auckland"
+	},
+
+	"Canterbury": {
+		"money_needed": 36500,
+		"grades_needed": 75,
+		"location": "Christchurch"
+	},
+
+	"Waikato": {
+		"money_needed": 27000,
+		"grades_needed": 70,
+		"location": "Hamilton"
+	}
+}
--- main-game/Backend/Intro/university_select.gd
+++ main-game/Backend/Intro/university_select.gd
@@ -1,8 +1,35 @@
 extends Node2D
 
+@onready var Auckland = $Auckland
+@onready var Canterbury = $Canterbury
 
 
 func _on_button_pressed() -> void:
 	MusicManager.pause_music()
 	get_tree().change_scene_to_file("res://Main_Game/Scenes/MainMap.tscn")
 	Inventory.get_node("UI").show()
+
+
+func _on_confirm_pressed() -> void:
+	GameBackend.selected_university = "Auckland"
+	MusicManager.pause_music()
+	get_tree().change_scene_to_file("res://Main_Game/Scenes/MainMap.tscn")
+	Inventory.get_node("UI").show()
+	
+
+
+func _on_auckland_pressed() -> void:
+	Auckland.show()
+	Canterbury.hide()
+
+
+func _on_confirm_2_pressed() -> void:
+	GameBackend.selected_university = "Canterbury"
+	MusicManager.pause_music()
+	get_tree().change_scene_to_file("res://Main_Game/Scenes/MainMap.tscn")
+	Inventory.get_node("UI").show()
+	
+	
+func _on_canterbury_pressed() -> void:
+	Auckland.hide()
+	Canterbury.show()
